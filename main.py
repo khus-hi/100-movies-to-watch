6684ebac78a69ef33b6b3e6ce6de1e07bee18d68
Khushi Teli <103373587+khus-hi@users.noreply.github.com>
2023-09-07 10:14:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+response=requests.get(URL)
+website_html=response.text
+
+soup=BeautifulSoup(website_html,"html.parser")
+all_movies=soup.find_all(name="h3",class_="title")
+movie_tiles=[movie.getText() for movie in all_movies]
+movies=movie_tiles[::-1]
+
+with open("movies.text",mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
